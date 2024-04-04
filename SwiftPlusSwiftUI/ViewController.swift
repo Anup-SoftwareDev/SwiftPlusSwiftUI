29ab7fbb0b30541ddf33276e23c98e7539d64135
anup <joseph.anupsa>
2024-04-04 16:51:10 +1100

    LabelInClone created

--- SwiftPlusSwiftUI/ViewController.swift
+++ SwiftPlusSwiftUI/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let label = UILabel()
+    let labelInClone = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,14 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        view.addSubview(labelInClone)
+        labelInClone.text = "LabelInClone"
+        labelInClone.textColor = .green
+        labelInClone.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            labelInClone.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelInClone.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40)
         ])
     }
 
